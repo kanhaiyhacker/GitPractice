879c584bd1a83d49d5cfe3464b4325700030a9a2
Kanhaiya Adhikari <kadhikari@aapnainfotech.com>
2019-04-15 15:16:38 +0530

    commit change in content_toolbar

--- app/src/main/java/com/e/myapplication/MainActivity.java
+++ app/src/main/java/com/e/myapplication/MainActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    ShowText showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        showText = new ShowText();
+
     }
 
 }
---
+++ app/src/main/java/com/e/myapplication/ShowText.java
@@ -0,0 +1,4 @@
+package com.e.myapplication;
+
+public class ShowText {
+}
